aee4352f4e88a7ffea360a893a64f035253a8570
AlanGrg77 <131133200+AlanGrg77@users.noreply.github.com>
2024-12-29 12:55:13 +0545

    removing cache node_modules and .env

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const connectToDatabase = require('./database/index');
+const Blog = require('./model/blogModel');
 const app = express();
+
+app.use(express.json())
 
 connectToDatabase()
 
@@ -13,6 +16,27 @@ app.get('/',(req,res)=>{
             message:'Hello World'
         }
     )
+})
+
+app.post('/blog', async (req,res)=>{
+    const {title,subtitle,description,image} = req.body;
+    if(!title || !subtitle || !description || !image){
+        return res.status(400).json(
+            {
+                message : 'Please enter title, subtitle, description and image.'
+            }
+        )
+    }
+
+    await Blog.create({
+        title : title,
+        subtitle : subtitle,
+        description : description,
+        image : image
+    });
+    res.status(200).json({
+        message : 'Blog api hit'
+    })
 })
 
 app.listen(process.env.PORT,()=>{
---
+++ model/blogModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    title: {
+        type:String,
+        unique:true
+    },
+    subTitle:{
+        type:String
+    },
+    description:{
+        type:String
+    },
+    image:{
+        type:String
+    }
+});
+
+const Blog = mongoose.model('Blog',blogSchema);
+module.exports = Blog;
