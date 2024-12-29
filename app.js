cd976f3e0fcd2cfbd0287b3d45e48051d972e3d8
AlanGrg77 <131133200+AlanGrg77@users.noreply.github.com>
2024-12-29 23:45:20 +0545

    Day 12 req.file,get blog

--- app.js
+++ app.js
@@ -21,25 +21,39 @@ app.get('/',(req,res)=>{
 })
 
 app.post('/blog',upload.single("image"), async (req,res)=>{
-    const {title,subtitle,description,image} = req.body;
-    if(!title || !subtitle || !description || !image){
+    const {title,subtitle,description} = req.body;
+    const filename = req.file.filename;
+    console.log(req.body)
+    console.log(req.file)
+    if(!title || !subtitle || !description){
         return res.status(400).json(
             {
-                message : 'Please enter title, subtitle, description and image.'
+                message : 'Please enter title, subtitle, description'
             }
         )
     }
+    
 
     await Blog.create({
         title : title,
         subtitle : subtitle,
         description : description,
-        image : image
+        image : filename
     });
     res.status(200).json({
         message : 'Blog api hit'
     })
 })
+
+app.get('/blog',async (req,res)=>{
+   const blogs = await Blog.find();
+   res.status(200).json({
+    message: 'data fetched successfully',
+    data : blogs
+   })
+})
+
+app.use(express.static('./storage'))
 
 app.listen(process.env.PORT,()=>{
     console.log("Hello node");
