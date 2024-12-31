3269234c24af994b2683bb7f16d596680ddcf7a0
AlanGrg77 <131133200+AlanGrg77@users.noreply.github.com>
2024-12-31 20:32:04 +0545

    CRUD in node

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const connectToDatabase = require('./database/index');
 const Blog = require('./model/blogModel');
 const { storage, multer } = require('./middleware/multerConfig');
 const app = express();
+const fs = require('fs');
 
 app.use(express.json())
 
@@ -51,6 +52,61 @@ app.get('/blog',async (req,res)=>{
     message: 'data fetched successfully',
     data : blogs
    })
+})
+
+app.get('/blog/:id',async (req,res)=>{
+    const {id} = req.params;
+    const blog = await Blog.findById(id);
+    res.status(200).json({
+        message : 'api hit',
+        data : blog 
+    })
+})
+
+app.delete('/blog/:id', async(req,res)=>{
+    const {id} = req.params;
+    const blog = await Blog.findById(id);
+    const ImageName = blog.image;
+    fs.unlink(`./storage/${ImageName}`,(err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log('deleted successfully')
+        }
+    })
+    await Blog.findByIdAndDelete(id);
+    res.status(200).json({
+        message : 'deleted successfully'
+    })
+})
+
+app.patch('/blog/:id',upload.single('image'), async (req,res)=>{
+    const {id} = req.params;
+    const {title, subtitle, description } = req.body;
+    const blog = Blog.findById(id)
+    let ImageName = blog.image; //if no change in image
+    if(req.file){
+        ImageName = req.file.filename; // if there is change in image then take the filename from req.file
+        const OldImageName = blog.image;
+        fs.unlink(`./storage/${OldImageName}`,(err)=>{
+            if(err){
+                console.log(err);
+            }else{
+                console.log('deleted successfully')
+            }
+        });                                             
+    } 
+    
+    await Blog.findByIdAndUpdate(id,{
+        title : title,
+        subtitle : subtitle,
+        description : description,
+        image : ImageName
+    })
+
+    res.status(200).json({
+        message: 'updated successfully'
+    });
 })
 
 app.use(express.static('./storage'))
